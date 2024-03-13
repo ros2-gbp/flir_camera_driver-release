fdc88e498ca1a04d473a36cc0b867f7e16deb12d
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2024-03-13 09:37:09 -0400

    Rebase from 'upstream'

--- spinnaker_camera_driver/include/spinnaker_camera_driver/exposure_controller.hpp
+++ spinnaker_camera_driver/include/spinnaker_camera_driver/exposure_controller.hpp
@@ -1,5 +1,5 @@
 // -*-c++-*--------------------------------------------------------------------
-// Copyright 2023 Bernd Pfrommer <bernd.pfrommer@gmail.com>
+// Copyright 2024 Bernd Pfrommer <bernd.pfrommer@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,25 +13,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef SPINNAKER_CAMERA_DRIVER__CAMERA_DRIVER_HPP_
-#define SPINNAKER_CAMERA_DRIVER__CAMERA_DRIVER_HPP_
+#ifndef SPINNAKER_CAMERA_DRIVER__EXPOSURE_CONTROLLER_HPP_
+#define SPINNAKER_CAMERA_DRIVER__EXPOSURE_CONTROLLER_HPP_
 
-#include <image_transport/image_transport.hpp>
 #include <memory>
-#include <rclcpp/rclcpp.hpp>
-#include <spinnaker_camera_driver/camera.hpp>
 
 namespace spinnaker_camera_driver
 {
-class CameraDriver : public rclcpp::Node
+class Image;
+class Camera;
+
+class ExposureController
 {
 public:
-  explicit CameraDriver(const rclcpp::NodeOptions & options);
-  ~CameraDriver();
-
-private:
-  std::shared_ptr<image_transport::ImageTransport> imageTransport_;
-  std::shared_ptr<Camera> camera_;
+  ExposureController() = default;
+  virtual ~ExposureController() {}
+  virtual void update(Camera * cam, const std::shared_ptr<const Image> & img) = 0;
+  virtual void addCamera(const std::shared_ptr<Camera> & cam) = 0;
 };
 }  // namespace spinnaker_camera_driver
-#endif  // SPINNAKER_CAMERA_DRIVER__CAMERA_DRIVER_HPP_
+#endif  // SPINNAKER_CAMERA_DRIVER__EXPOSURE_CONTROLLER_HPP_
