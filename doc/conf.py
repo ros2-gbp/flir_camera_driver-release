3db940e8c10e9a37b0e7815fb0ab5af4c1637ba2
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2025-05-25 13:12:21 -0400

    Revert "Trimmed the branch to only the spinnaker_synchronized_camera_driver sub directory"
    This reverts commit 41bc9d50a8a3532e66f937f11357076223c6f845.

--- doc/conf.py
+++ doc/conf.py
@@ -1,54 +1,52 @@
-# Copyright 2024 Bernd Pfrommer
+# Configuration file for the Sphinx documentation builder.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-project = 'spinnaker_camera_driver'
-# copyright = '2024, Bernd Pfrommer'
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'FLIR camera drivers'
+copyright = '2024, Bernd Pfrommer'
 author = 'Bernd Pfrommer'
 
 
-# Add any Sphinx extension module names here, as strings.
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    'myst_parser',
-    'sphinx.ext.doctest',
-    'sphinx_rtd_theme',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-source_suffix = '.rst'
-# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-exclude_patterns = []
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
-htmlhelp_basename = 'spinnaker_camera_driver_doc'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-
-# html_static_path = ['_static']
+html_static_path = ['_static']
